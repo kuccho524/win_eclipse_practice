d8b13f2c1ad3c4b9e5599ad280c2e7c0b5dbc4af
kuccho524 <kucchachachanocha@gmail.com>
2021-08-13 19:25:29 +0900

    practice

---
+++ src/com/jp/practice/Practice1.java
@@ -0,0 +1,18 @@
+package com.jp.practice;
+
+/**
+ *
+ * @author kuccho
+ * 【数あてゲーム】
+ *
+ */
+
+public class Practice1 {
+	public static void main(String[] args) {
+
+		String greet = new java.util.Scanner(System.in).nextLine();
+
+		System.out.println(greet);
+
+	}
+}
